bc6cd9a7f7c60f3c32d1a3fa34b4beff147ff40d
Felixoofed <69742512+Felixoofed@users.noreply.github.com>
2022-02-15 12:55:06 +0800

    mobile friendly drawering

--- draw/app.js
+++ draw/app.js
@@ -1,40 +1,83 @@
-var canvas = document.getElementById('canvas');
-var context = canvas.getContext('2d');
-
-var radius = 5;
-var start = 0;
-var end = Math.PI * 2;
-var dragging = false;
-
-canvas.width = 800;
-canvas.height = 600;
-
-context.lineWidth = radius * 2;
-
-var putPoint = function(e){
-	if(dragging){
-		context.lineTo(e.offsetX, e.offsetY);
-		context.strokeStyle = document.getElementById('colorpicker').value;
-		context.fillStyle = document.getElementById('colorpicker').value;
-		context.stroke();
-		context.beginPath();
-		context.arc(e.offsetX, e.offsetY, radius, start, end);
-		context.fill();
-		context.beginPath();
-		context.moveTo(e.offsetX, e.offsetY);
+window.onload = function() {
+	var canvas = document.getElementById("canvas");
+	var context = canvas.getContext("2d");
+	
+    canvas.width = 800;
+	canvas.height = 600;
+	
+	// Set Background Color
+    context.fillStyle="#fff";
+    context.fillRect(0,0,canvas.width,canvas.height);
+	
+    // Mouse Event Handlers
+	if(canvas){
+		var isDown = false;
+		var canvasX, canvasY;
+		context.lineWidth = 3;
+		
+		$(canvas)
+		.mousedown(function(e){
+			isDown = true;
+			context.beginPath();
+			canvasX = e.pageX - canvas.offsetLeft;
+			canvasY = e.pageY - canvas.offsetTop;
+			context.moveTo(canvasX, canvasY);
+		})
+		.mousemove(function(e){
+			if(isDown !== false) {
+				canvasX = e.pageX - canvas.offsetLeft;
+				canvasY = e.pageY - canvas.offsetTop;
+				context.lineTo(canvasX, canvasY);
+				context.strokeStyle = "#000";
+				context.stroke();
+			}
+		})
+		.mouseup(function(e){
+			isDown = false;
+			context.closePath();
+		});
 	}
-}
+	
+	// Touch Events Handlers
+	draw = {
+		started: false,
+		start: function(e) {
 
-var focused = function(e){
-	dragging = true;
-	putPoint(e);
-}
+			context.beginPath();
+			context.moveTo(
+				e.touches[0].pageX,
+				e.touches[0].pageY
+			);
 
-var unfocused = function(){
-	dragging = false;
-	context.beginPath();
-}
+			this.started = true;
 
-canvas.addEventListener('mousedown', focused);
-canvas.addEventListener('mousemove', putPoint);
-canvas.addEventListener('mouseup', unfocused);
+		},
+		move: function(e) {
+
+			if (this.started) {
+				context.lineTo(
+					e.touches[0].pageX,
+					e.touches[0].pageY
+				);
+
+				context.strokeStyle = "#000";
+				context.lineWidth = 5;
+				context.stroke();
+			}
+
+		},
+		end: function(e) {
+			this.started = false;
+		}
+	};
+	
+	// Touch Events
+	canvas.addEventListener('touchstart', draw.start, false);
+	canvas.addEventListener('touchend', draw.end, false);
+	canvas.addEventListener('touchmove', draw.move, false);
+	
+	// Disable Page Move
+	document.body.addEventListener('touchmove',function(e){
+		e.preventDefault();
+	},false);
+};
